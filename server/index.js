216ed55a4f234c1e41f59ca50ae63f9d2f5f28ba
Dan DeGreef <daniel_degreef@condenast.com>
2017-11-05 14:36:23 -0600

    make require work

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const Game = require('./game');
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-let game = new Game();
+let game = new Game(10,10);
 
 io.on('connection', function (socket) {
   socket.on('join', function (msg) {
